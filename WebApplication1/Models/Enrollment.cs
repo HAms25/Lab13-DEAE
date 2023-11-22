decc1ed40445c189730516ec934b44b0d9ebd6b4
Alumno <Alumno@LAB1504-28>
2023-11-21 21:55:38 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public DateTime DateTime { get; set; }
+
+        //Llaves foráneas
+        public Course? Course { get; set; }
+        public int? CourseID { get; set; }
+        public Student? Student { get; set; }
+        public int? StudentID { get; set; }
+    }
+}
